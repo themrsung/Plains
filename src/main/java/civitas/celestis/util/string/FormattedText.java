d149012e507bb5649409f685b0ac470a5ba1a864
themrsung <themrsung@gmail.com>
2023-08-19 12:27:07 +0900

    Stuff

--- src/main/java/civitas/celestis/util/string/FormattedText.java
+++ src/main/java/civitas/celestis/util/string/FormattedText.java
@@ -1,9 +1,7 @@
 package civitas.celestis.util.string;
 
+import civitas.celestis.graphics.Color8;
 import jakarta.annotation.Nonnull;
-
-import java.util.Arrays;
-import java.util.EnumSet;
 
 /**
  * A formatable {@link Text} object.
@@ -15,106 +13,12 @@ public interface FormattedText extends Text {
     //
 
     /**
-     * Returns the formatting of this text.
+     * Returns the color of this text.
      *
-     * @return The formatting of this text
+     * @return The color of this text
      */
     @Nonnull
-    Format format();
+    Color8 color();
 
-    //
-    // Formatting
-    //
 
-    /**
-     * Creates a new {@link Format} object from the provided array of options.
-     *
-     * @param options The options to construct the format with
-     * @return The constructed {@link Format} object
-     */
-    @Nonnull
-    static Format newFormat(@Nonnull Format.Option... options) {
-        return new Format(options);
-    }
-
-    /**
-     * The formatting of a {@link Text} object.
-     */
-    class Format {
-        //
-        // Options
-        //
-
-        /**
-         * The available options for formatting a {@link Text} object.
-         */
-        public enum Option {
-            BOLD("b"),
-            ITALIC("i"),
-            UNDERLINED("u"),
-            STRIKE("s");
-
-            /**
-             * Creates a new option.
-             *
-             * @param tag The tag of this option
-             */
-            Option(@Nonnull String tag) {
-                this.tag = tag;
-            }
-
-            /**
-             * The tag of this option.
-             */
-            @Nonnull
-            private final String tag;
-
-            /**
-             * Returns the tag of this option.
-             *
-             * @return The tag ot this option
-             */
-            @Nonnull
-            public String tag() {
-                return tag;
-            }
-        }
-
-        //
-        // Variables
-        //
-
-        /**
-         * The set of options this format contains.
-         */
-        @Nonnull
-        private final EnumSet<Option> options;
-
-        //
-        // Constructors
-        //
-
-        /**
-         * Creates a new {@link Format} object.
-         *
-         * @param options The options of this format
-         */
-        protected Format(@Nonnull Option... options) {
-            this.options = EnumSet.copyOf(Arrays.stream(options).toList());
-        }
-
-        //
-        // Getters
-        //
-
-        /**
-         * Returns a list of options this format has.
-         *
-         * @return A list of options this format has
-         */
-        @Nonnull
-        public EnumSet<Option> options() {
-            return options;
-        }
-    }
 }
