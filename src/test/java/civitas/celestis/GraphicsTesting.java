af28598b4312fddac47950dc4b7a02227b216bd2
themrsung <themrsung@gmail.com>
2023-08-22 16:55:50 +0900

    trinity

--- src/test/java/civitas/celestis/GraphicsTesting.java
+++ src/test/java/civitas/celestis/GraphicsTesting.java
@@ -1,50 +1,5 @@
 package civitas.celestis;
 
-import civitas.celestis.graphics.Colors;
-import civitas.celestis.graphics.LinearColor;
-import civitas.celestis.graphics.XFrame;
-import civitas.celestis.graphics.XPanel;
-import civitas.celestis.task.Scheduler;
-import civitas.celestis.task.SyncScheduler;
-
-import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.util.concurrent.atomic.AtomicReference;
-
 public class GraphicsTesting {
-    static AtomicReference<LinearColor> color = new AtomicReference<>(LinearColor.CYAN);
-    static XFrame frame = new XFrame("Test");
-    static XPanel panel = new XPanel() {
-        @Override
-        public void paint(Graphics g) {
-            g.setColor(color.get());
-            g.fillRect(0, 0, getWidth(), getHeight());
-            g.dispose();
-        }
-    };
-    static Scheduler scheduler = new SyncScheduler();
 
-    public static void main(String[] args) {
-        scheduler.register(delta -> {
-            color.getAndUpdate(c -> c.lerp(Colors.random(), 0.005));
-            frame.repaint();
-        });
-
-        frame.setSize(1920, 1080);
-
-        frame.add(panel);
-
-        frame.setVisible(true);
-        scheduler.start();
-
-        frame.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                frame.dispose();
-                scheduler.interrupt();
-                System.exit(0);
-            }
-        });
-    }
 }
--- src/test/java/civitas/celestis/GroupTesting.java
+++ src/test/java/civitas/celestis/GroupTesting.java
@@ -1,13 +1,7 @@
 package civitas.celestis;
 
-import civitas.celestis.util.array.SafeArray;
-
 public class GroupTesting {
     public static void main(String[] args) {
-        final SafeArray<String> array = SafeArray.of("reds", "aslddask", "rredkr", "asdsdsddklk");
-        System.out.println(array.cast(Object.class));
-
-        System.out.println(array.arrayList().array().arrayList());
 
     }
 }
