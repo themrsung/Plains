d7ce1ed49092ecc3235a44389f0fbac1d2cde5a2
themrsung <themrsung@gmail.com>
2023-08-22 17:10:10 +0900

    stuff

--- src/main/java/civitas/celestis/math/Scalars.java
+++ src/main/java/civitas/celestis/math/Scalars.java
@@ -317,4 +317,106 @@ public final class Scalars {
         return max;
     }
 
+    //
+    //
+    //
+    // Gamma Function
+    //
+    //
+    //
+
+    // Lanczos parameters
+    private static final double LANCZOS_G = 7;
+    private static final double[] LANCZOS_COEFFICIENTS = {
+            0.99999999999980993,
+            676.5203681218851,
+            -1259.1392167224028,
+            771.32342877765313,
+            -176.61502916214059,
+            12.507343278686905,
+            -0.13857109526572012,
+            9.9843695780195716e-6,
+            1.5056327351493116e-7
+    };
+
+    /**
+     * Returns the gamma of the given input {@code n}.
+     *
+     * @param n The value to get the gamma of
+     * @return The gamma function value of {@code n}
+     */
+    public static double gamma(double n) {
+        // Copy value for manipulation
+        double x = n;
+
+        if (x <= 0.5) {
+            return Math.PI / (Math.sin(Math.PI * x) * gamma(1 - x));
+        }
+
+        x -= 1;
+        double result = LANCZOS_COEFFICIENTS[0];
+        for (int i = 1; i < LANCZOS_COEFFICIENTS.length; i++) {
+            result += LANCZOS_COEFFICIENTS[i] / (x + i);
+        }
+
+        final double t = x + LANCZOS_G + 0.5;
+        return Math.sqrt(2 * Math.PI) * Math.pow(t, x + 0.5) * Math.exp(-t) * result;
+    }
+
+    //
+    //
+    //
+    // Factorial
+    //
+    //
+    //
+
+    /**
+     * Calculates the factorial of the provided input {@code n}.
+     * Note that this requires a non-negative input.
+     *
+     * @param n The number to get the factorial of
+     * @return The factorial of {@code n}
+     */
+    public static double factorial(double n) {
+        // Estimate factorial using the Gamma function
+        return gamma(n + 1);
+    }
+
+    /**
+     * Calculates the factorial of the provided input {@code n}.
+     * Note that this requires a non-negative input. Values over {@code 20} will trigger
+     * an integer overflow.
+     *
+     * @param n The number to get the factorial of
+     * @return The factorial of {@code n}
+     */
+    public static long factorial(long n) {
+        return n < FACTORIALS.length ? FACTORIALS[(int) n] : (long) factorial((double) n);
+    }
+
+    // The pre-computed table of integer factorials.
+    private static final long[] FACTORIALS = {
+            1L,
+            1L,
+            2L,
+            6L,
+            24L,
+            120L,
+            720L,
+            5040L,
+            40320L,
+            362880L,
+            3628800L,
+            39916800L,
+            479001600L,
+            6227020800L,
+            87178291200L,
+            1307674368000L,
+            20922789888000L,
+            355687428096000L,
+            6402373705728000L,
+            121645100408832000L,
+            2432902008176640000L
+    };
 }
